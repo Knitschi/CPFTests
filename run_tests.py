ee35ea7fc8ede80f7bf674e84c71320dcf7d0026
Knitschi <knitschi@freenet.de>
2018-04-30 18:14:40 +0200

    Add tests for 1_Configure.py script. Allow running subsets of CPFTests by setting the CPFTESTS_TEST_NAME variable.

--- run_tests.py
+++ run_tests.py
@@ -3,8 +3,11 @@
 Runs all high-level tests of the CMakeProjectFramework.
 
 Arguments:
-1. The test files directory.
-2. The configuration from which the current config derives.
+The script takes keyword arguments in the form
+
+test_dir="C:/mytests" -> A base directory in which the script can put temporary files for tests.
+parent_config=VS      -> The configuration from which the current config derives. Testprojects will be build in this configuration.
+test_name             -> Determines the tests that is run. If the value is empty, it will run all tests.
 """
 
 import unittest
@@ -19,10 +22,57 @@ from .misc_tests import *
 from .simpleonelibcpftestproject_tests import *
 
 
+def parseKeyWordArgs( arglist ):
+    """
+    The function takes a list of strings of the shape
+    bla=2 bli='blub' and sets fills a dictionary with
+    them which then is returned.
+    """
+    arg_dict = {}
+    for arg in arglist[1:]:
+        # check that argument has the equal sign
+        index = arg.find('=')
+        if index == -1:
+            raise Exception('Argument "{0}" does not seem to be a keyword argument.'.format(arg))
+        # check that argument has a keyword.
+        if index == 0:
+            raise Exception('Argument "{0}" is missing a keyword before the "=".'.format(arg))
+        
+        keyword = arg[:index]
+        value = arg[index+1:]
+
+        # strip quotes from value
+        firstchar = value[:1]
+        if firstchar == "'":
+            value = value.strip("'")
+        elif firstchar == '"':
+            value = value.strip('"')
+
+        arg_dict[keyword] = value
+
+    return arg_dict
+
+def getKeywordArgument(keyword, keywordargs):
+    if not keyword in keywordargs:
+        raise Exception('Test script requires {0} argument.'.format(keyword))
+    return keywordargs[keyword]
+
+
 if __name__ == '__main__':
-    testprojectfixture.BASE_TEST_DIR = sys.argv[1]
-    testprojectfixture.PARENT_CONFIG = sys.argv[2]
-    del(sys.argv[1])
-    del(sys.argv[1])
-    unittest.main()
+
+    # Get the script arguments
+    keywordargs = parseKeyWordArgs(sys.argv)
+    testprojectfixture.BASE_TEST_DIR = getKeywordArgument('test_dir', keywordargs)
+    testprojectfixture.PARENT_CONFIG = getKeywordArgument('parent_config', keywordargs)
+    test_name = getKeywordArgument('test_name', keywordargs)
+    # run all tests by default
+    if test_name == '':
+        test_name = 'run_tests'
+
+    # Run the tests
+    test_loader = unittest.TestLoader()
+    suite = test_loader.loadTestsFromName('Sources.CPFTests.' + test_name)
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
+
 
--- simpleonelibcpftestproject_tests.py
+++ simpleonelibcpftestproject_tests.py
@@ -44,5 +44,26 @@ class SimpleOneLibCPFTestProjectFixture(testprojectfixture.TestProjectFixture):
             self.assertIn('MSBUILD : error MSB1009:', cm.exception.stdout)
 
 
+    def test_configure_script(self):
+        """
+        This tests the underlying functionality of the '1_Configure.py' script.
+        """
+        # SETUP
+        self.run_python_command('Sources/CPFBuildscripts/0_CopyScripts.py')
+
+        # EXECUTE
+        # Check that a failed call causes an test exception.
+        self.assertRaises(miscosaccess.CalledProcessError, self.run_python_command, '1_Configure.py {0}'.format(testprojectfixture.PARENT_CONFIG))
+
+        # Check that configuring an existing variable works.
+        # Test overriding existing variables and setting variables with whitespaces.
+        self.run_python_command('1_Configure.py MyConfig --inherits {0} -D CPF_ENABLE_DOXYGEN_TARGET=OFF -D BLUB="bli bla"'.format(testprojectfixture.PARENT_CONFIG))
+        self.run_python_command('2_Generate.py')
+        cmakeCacheVariables = self.osa.execute_command_output('cmake Generated/MyConfig -L', cwd=self.test_dir, print_output=False, print_command=False)
+        # Note that variables that are added via 1_Configure.py are always of type string
+        self.assertIn('CPF_ENABLE_DOXYGEN_TARGET:STRING=OFF', cmakeCacheVariables)
+        self.assertIn('BLUB:STRING=bli bla', cmakeCacheVariables)
+
+        
 
 
