9ee73b17ea61e99d11fd13499f6e67d2b9210972
Knitschi <11889116+Knitschi@users.noreply.github.com>
2018-10-21 11:41:39 +0200

    Add a test to check if a library that was installed via install target can be consumed by another project.

--- distpackageconsumption_tests.py
+++ distpackageconsumption_tests.py
@@ -60,8 +60,43 @@ class DistPackageFixture(unittest.TestCase):
     def get_package_file_we(self, version, compilerConfig):
         return "MyLib.{0}.{1}.dev.{2}".format(version, self.osa.system(), compilerConfig)
 
+    def assert_consumer_project_builds_and_runs(self):
+        """
+        This does the following checks.
+        - Check the CPFPackageConsumerTestProject builds.
+        - Check the MyLibConsumer executable runs without error.
+        - Check that .pdb files from the consumed libraries are deployed into the build-tree.
+        """
+        # Check that the deployment of the external dll works by running the executable
+        consumerVersion = self.consumerProjectFixture.get_package_version('MyLibConsumer')
+        consumerExe = self.consumerProjectFixture.get_package_executable_path_in_build_tree(
+            'MyLibConsumer',
+            testprojectfixture.PARENT_CONFIG,
+            testprojectfixture.COMPILER_CONFIG,
+            consumerVersion
+        )
+        self.osa.execute_command_output(
+            str(consumerExe),
+            cwd=self.consumerProjectFixture.cpf_root_dir,
+            print_output=miscosaccess.OutputMode.ON_ERROR
+        )
 
-    def test_consume_the_simple_test_project_library(self):
+        # Assert .pdb files are deployed in debug configuration when MyLib is a shared library.
+        # This tests the pdb deployment for imported targets.
+        if self.consumerProjectFixture.is_shared_libraries_config() and self.consumerProjectFixture.is_windows():
+            debugConfig = "Debug"
+            binaryOutputDirConfig = self.consumerProjectFixture.locations.get_full_path_binary_output_folder(
+                "MyLibConsumer",
+                testprojectfixture.PARENT_CONFIG,
+                debugConfig
+            )
+
+            pdbFileLib = binaryOutputDirConfig / "MyLib-{0}.pdb".format(debugConfig.lower())
+            pdbFileFixtureLib = binaryOutputDirConfig / "MyLib_fixtures-{0}.pdb".format(debugConfig.lower())
+            self.consumerProjectFixture.assert_files_exist([pdbFileLib, pdbFileFixtureLib])
+
+
+    def test_consume_the_simple_test_project_library_from_distribution_package(self):
         """
         This test builds the distribution packages of the SimpleOneLibCPFTestProject
         and tests for one of them if the package can be used within the CPFPackageConsumerTestProject.
@@ -110,34 +145,44 @@ class DistPackageFixture(unittest.TestCase):
 
 
         # ------------------- Verify -----------------------
-
-        # Check that the deployment of the external dll works by running the executable
-        consumerVersion = self.consumerProjectFixture.get_package_version('MyLibConsumer')
-        consumerExe = self.consumerProjectFixture.get_package_executable_path_in_build_tree(
-            'MyLibConsumer',
-            testprojectfixture.PARENT_CONFIG,
-            testprojectfixture.COMPILER_CONFIG,
-            consumerVersion
-        )
-        self.osa.execute_command_output(
-            str(consumerExe),
-            cwd=self.consumerProjectFixture.cpf_root_dir,
-            print_output=miscosaccess.OutputMode.ON_ERROR
-        )
-
-        # Assert .pdb files are deployed in debug configuration when MyLib is a shared library.
-        # This tests the pdb deployment for imported targets.
-        if self.consumerProjectFixture.is_shared_libraries_config() and self.consumerProjectFixture.is_windows():
-            debugConfig = "Debug"
-            binaryOutputDirConfig = self.consumerProjectFixture.locations.get_full_path_binary_output_folder(
-                "MyLibConsumer",
-                testprojectfixture.PARENT_CONFIG,
-                debugConfig
-            )
-
-            pdbFileLib = binaryOutputDirConfig / "MyLib-{0}.pdb".format(debugConfig.lower())
-            pdbFileFixtureLib = binaryOutputDirConfig / "MyLib_fixtures-{0}.pdb".format(debugConfig.lower())
-            self.consumerProjectFixture.assert_files_exist([pdbFileLib, pdbFileFixtureLib])
+        self.assert_consumer_project_builds_and_runs()
 
 
+
+
+    def test_consume_the_simple_test_project_library_from_direct_install(self):
+        """
+        This test executes the install target of the SimpleOneLibCPFTestProject to copy
+        the MyLib package to the CPFPacakgeConsumerTestProject and then checks if
+        the consumer project can be build and run.
+        """
+        # ------------------- Setup -----------------------
+
+        # Execute the install target for each compiler.
+        installPrefix = self.consumerProjectFixture.cpf_root_dir / "install"
+        self.libraryProjectFixture.generate_project(d_options=['CMAKE_INSTALL_PREFIX={0}'.format(installPrefix)])
+        compilerConfigs = self.libraryProjectFixture.get_compiler_configs()
+        myLibVersion = self.libraryProjectFixture.get_package_version("MyLib")
+
+        for config in compilerConfigs:
+            # Build and install the library project
+            self.libraryProjectFixture.build_target("install", config=config)
+
+        # Delete the library project to make sure unwanted references to
+        # the original directory cause errors.
+        self.fsa.rmtree(self.libraryProjectFixture.cpf_root_dir)
+
+
+        # ------------------- Execute -----------------------
+
+        # Build the consumer project.
+        packageFileWE = self.get_package_file_we(myLibVersion, testprojectfixture.COMPILER_CONFIG)
+        self.consumerProjectFixture.generate_project([
+            "MYLIB_VERSION={0}".format(myLibVersion),
+            "MYLIB_LOCATION={0}".format(installPrefix)
+            ])
+        self.consumerProjectFixture.build_target("MyLibConsumer")
+
+        # ------------------- Verify -----------------------
+        self.assert_consumer_project_builds_and_runs()
 
